2b48686ef22a278dec6accc96383f1e670502763
Eytan Kaplan <eytankaplan101@gmail.com>
2020-11-06 17:00:45 -0500

    Updated Get_character_name_race

--- SQL/Get_character_name_race.sql
+++ SQL/Get_character_name_race.sql
@@ -3,6 +3,6 @@ SQLDnD.Character.characterName,
 SQLDnD.Race.raceName 
 
 FROM SQLDnD.Character
-JOIN SQLDnD.Character_has_Race ON SQLDnD.Character.idCharacter = SQLDnD.Character_has_Race.Character_idCharacter
-JOIN SQLDnD.Race ON SQLDnD.Character_has_Race.Race_idRace = SQLDnD.Race.idRace
+JOIN SQLDnD.Race ON SQLDnD.Character.Race_idRace = SQLDnD.Race.idRace
+ORDER BY SQLDnD.Character.characterName DESC
 ;
